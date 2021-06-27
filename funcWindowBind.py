f3807e2e6c3aa58e450ad0ceb28c502efc037fd4
dragonbra <tommy514@foxmail.com>
2021-06-27 22:59:47 +0800

    实现批量预测功能和导出结果

--- funcWindowBind.py
+++ funcWindowBind.py
@@ -1,5 +1,6 @@
 import time
 
+import pandas as pd
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
@@ -32,8 +33,12 @@ class funcWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         # multiPredict Widget's Function
         self.multiNewsSize = 0
+        self.multiNewsTable.setColumnWidth(0, 300)
+        self.multiNewsTable.setColumnWidth(1, 500)
         self.multiFileButton.clicked.connect(self.multiGetNewsFromFile)
         self.multiPredictButton.clicked.connect(self.multiNewsPredict)
+
+        self.multiSaveButton.clicked.connect(self.multiNewsSave)
 
     def singleGetNewsFromFile(self):
         filePath, fileType = QtWidgets.QFileDialog.getOpenFileName(self, '选择文件', self.defaultFilePath, "单新闻文件(*.txt)")
@@ -68,34 +73,82 @@ class funcWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def multiNewsInsertTableWidget(self, index, channelName, newsTitle, newsContent):
         # TODO
         # Insert news into table widget in multiNewsWidget
-        self.multiNewsTable.insertRow(self.multiNewsTable.rowCount())
+        if index != 0:
+            self.multiNewsTable.insertRow(index)
+        self.multiNewsTable.setItem(index, 0, QtWidgets.QTableWidgetItem(newsTitle))
+        self.multiNewsTable.setItem(index, 1, QtWidgets.QTableWidgetItem(newsContent))
+        # QApplication.processEvents()
         pass
 
     def multiGetNewsFromFile(self):
-        self.multiNewsTable.clear()
-        self.multiResultsTable.clear()
+        self.multiNewsTable.setRowCount(1)
+        self.multiNewsTable.clearContents()
+        self.multiResultsTable.setRowCount(1)
+        self.multiResultsTable.clearContents()
+
         filePath, fileType = QtWidgets.QFileDialog.getOpenFileName(self, '选择文件', self.defaultFilePath, "多新闻文件(*.csv, *.xls, *.xlsx)")
 
-        if not os.path.getsize(filePath):
+        print(filePath)
+        print(len(filePath))
+        if len(filePath) == 0:
             pass
 
-        try:
-            self.channelNameList, self.newsTitleList, self.newsContentList = dataProcUtils.readCorpus(filePath)
-            self.multiNewsSize = len(self.newsTitleList)
-            for index in range(self.multiNewsSize):
-                channelName = self.channelNameList[index]
-                newsTitle = self.newsTitleList[index]
-                newsContent = self.newsContentList[index]
-                self.multiNewsInsertTableWidget(index, channelName, newsTitle, newsContent)
+        else:
+            try:
+                self.channelNameList, self.newsTitleList, self.newsContentList = dataProcUtils.readCorpus(filePath)
+                self.multiNewsSize = len(self.newsTitleList)
+                for index in range(self.multiNewsSize):
+                    channelName = self.channelNameList[index]
+                    newsTitle = self.newsTitleList[index]
+                    newsContent = self.newsContentList[index]
+                    self.multiNewsInsertTableWidget(index, channelName, newsTitle, newsContent)
 
-        except Exception as e:
-            pass
+            except Exception as e:
+                pass
 
     def multiNewsPredict(self):
         if self.multiNewsSize == 0:
             return
         for index in range(self.multiNewsSize):
-            print(index)
+            newsTitle = self.multiNewsTable.item(index, 0).text()
+            newsContent = self.multiNewsTable.item(index, 1).text()
+            newsTextOri = str.strip(dataProcUtils.newsMerge(newsTitle, newsContent))
+
+            if index != 0:
+                self.multiResultsTable.insertRow(index)
+
+            if len(newsTextOri) == 0:
+                self.multiResultsTable.setItem(index, 0, "空")
+            else:
+                predictChannel = self.classifier.predict(newsTextOri)
+                self.multiResultsTable.setItem(index, 0, QtWidgets.QTableWidgetItem(predictChannel))
+
+            QApplication.processEvents()
+
+    def multiNewsSave(self):
+        filePath, fileType = QtWidgets.QFileDialog.getSaveFileName(self, "导出结果", self.defaultFilePath, ".csv文件(*.csv)")
+
+        try:
+            newsTitleList = []
+            newsContentList = []
+            channelNameList = []
+
+            for index in range(self.multiNewsSize):
+                newsTitle = self.multiNewsTable.item(index, 0).text()
+                newsContent = self.multiNewsTable.item(index, 1).text()
+                channelName = self.multiResultsTable.item(index, 0).text()
+
+                newsTitleList.append(newsTitle)
+                newsContentList.append(newsContent)
+                channelNameList.append(channelName)
+
+            output_excel = {'content': newsContentList, 'channelName': channelNameList, 'title': newsTitleList}
+            output = pd.DataFrame(output_excel)
+            print(filePath)
+            output.to_csv(filePath, index=False, encoding="gb18030", header=None)
+
+        except Exception as e:
+            pass
 
 
 if __name__ == '__main__':
